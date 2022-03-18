a2e20ef6745d6dc6d5f2d4f1da5dfcf585b11a41
harwcam <84109744+harwcam@users.noreply.github.com>
2022-03-17 22:55:02 -0400

    Added bootstrap aesthetics and drag functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,4 +169,61 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clome",
+  activate: function(event) {
+    console.log("activate", this)
+  },
+  deactivate: function(event) {
+    console.log("deactivate", event.target)
+  },
+  over: function(event) {
+    console.log("over", event.target)
+  },
+  out: function(event) {
+    console.log("out", event.target)
+  },
+  update: function(event) {
+    var tempArr = []
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+      console.log(text,date);
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    console.log(tempArr);
+    
+    var arrName = $(this)
+                  .attr("id")
+                  .replace("list-","");
+    tasks[arrName] = tempArr
+    saveTasks();
+  }
+});
 
+$('#trash').droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove()
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over")
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
